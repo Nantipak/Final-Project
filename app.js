05ea87d7f8e99d882f8f24735a51ce2a35ec1adc
nantipak tathong <auto0032@icloud.com>
2022-02-28 14:24:32 +0700

    Web design

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { default: mongoose } = require("mongoose");
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("home");
+})
+
+app.listen("3000", () => {
+    console.log("Server is running on Port 3000.");
+});

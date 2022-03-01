1e1d427bbdd4a586f945bc5c028ef3cb774f39ec
kittisak <kittisak_kae@cmu.ac.th>
2022-03-01 16:05:56 +0700

    add a link to all menu

--- app.js
+++ app.js
@@ -11,6 +11,22 @@ app.get("/", function(req, res) {
     res.render("home");
 })
 
+app.get("/appetizer", function(req, res) {
+    res.render("appetizer-menu");
+})
+
+app.get("/maindish", function(req, res) {
+    res.render("main-dish");
+})
+
+app.get("/dessert", function(req, res) {
+    res.render("dessert-menu");
+})
+
+app.get("/drinks", function(req, res) {
+    res.render("drinks-menu");
+})
+
 app.listen("3000", () => {
     console.log("Server is running on Port 3000.");
 });

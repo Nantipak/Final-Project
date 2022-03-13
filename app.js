3754507ba2b76670c612a228131f085ef1e23178
kittisak <kittisak_kae@cmu.ac.th>
2022-03-13 18:01:22 +0700

    create docker-compose

--- app.js
+++ app.js
@@ -80,6 +80,6 @@ app.post("/addFood", function(req, res) {
 
 const hostname = '0.0.0.0';
 const port = 3000;
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
 });
